8d2d13c8046ff75594263ea78986e6b0e811dac2
a-popova <papova.anastasiya.13@gmail.com>
2019-11-27 13:43:15 +0300

    feat: creat apikeys.js with keys and tokens for services

---
+++ src/javascript/apikeys.js
@@ -0,0 +1,10 @@
+let ipinfoToken = "0a1e251f6e7edb";
+
+let openWeatherAPIkey = "9601b1710c086a4da757c72411f6f712";
+
+let flickrAPIkey = "c877c808b443ac7713a520eb58ad0373"
+let flickrSecret = "7f06f9b9551717e6"
+
+let mapboxToken = "pk.eyJ1IjoiYS1wb3BvdmEiLCJhIjoiY2szaDVwbmd2MDhjbjNubXUxNHpwNXFwOSJ9.2iYfozV5wMpSlqzLNzbRYg";
+
+let opencagedataAPIkey = "6720512976cc457a8e0288e14a9ef3dd";
