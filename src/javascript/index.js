270f38744567452d405756742e3030cdfcedbd19
a-popova <papova.anastasiya.13@gmail.com>
2019-12-13 16:19:35 +0300

    fix: fix some eslint errors

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,8 @@
 import '../sass/styles.scss';
-import { type } from 'os';
 import {
   ipinfoToken, darkskyAPIkey, flickrAPIkey, mapboxToken, opencagedataAPIkey,
-} from './apikeys.js';
-import markup from './markup.js';
+} from './apikeys';
+import markup from './markup';
 import icons from './icons';
 import Translator from './translator';
 
@@ -24,37 +23,9 @@ window.onload = () => {
   image.classList.add('image');
 
   const city = document.querySelector('.header--cityInput input[name=city]');
-  city.addEventListener('click', () => recogniseSpeech());
-  city.addEventListener('keydown', (event) => {
-    if (event.keyCode === 13) {
-      getLocationByCity(city.value);
-    }
-  });
   const searchCityButton = document.querySelector('.header--cityInput input[class=search]');
-  searchCityButton.addEventListener('click', () => { getLocationByCity(city.value); city.value = ''; });
-
-  const languageButton = document.querySelector('select[class=languages]');
-  languageButton.addEventListener('change', () => { languageHandler(), false; });
-  languageButton.value = language.toUpperCase();
-
   const fahrButton = document.querySelector('.fahrenheit');
-  fahrButton.addEventListener('click', async () => { temperatureType = 'fahrenheit'; showCurrentForecast(await getForecast({ latitude, longitude })); });
   const celcButton = document.querySelector('.celcius');
-  celcButton.addEventListener('click', async () => { temperatureType = 'celcius'; showCurrentForecast(await getForecast({ latitude, longitude })); });
-
-  const refreshButton = document.querySelector('input[name=refresh]');
-  refreshButton.addEventListener('click', () => { loadImage(); });
-
-  setInterval(() => {
-    const dateInfo = extractDate(timezone);
-    showDate(dateInfo.utcDateArr, dateInfo.localDateArr);
-  }, 1000);
-
-  async function languageHandler() {
-    language = languageButton.value.toLowerCase();
-    localStorage.setItem('lang', language);
-    await getLocationByCoordinates(latitude, longitude);
-  }
 
   mapboxgl.accessToken = mapboxToken;
   const map = new mapboxgl.Map({
@@ -62,82 +33,6 @@ window.onload = () => {
     style: 'mapbox://styles/mapbox/streets-v11',
     zoom: 10,
   });
-
-  async function success(pos) {
-    if (pos) {
-      const crd = pos.coords;
-      const latitude = crd.latitude.toFixed(2);
-      const longitude = crd.longitude.toFixed(2);
-      await getLocationByCoordinates(latitude, longitude);
-    } else {
-      getPositionByIP();
-    }
-  }
-
-  navigator.geolocation.getCurrentPosition(success);
-
-  async function getLocationByCoordinates(latitude, longitude) {
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${opencagedataAPIkey}&language=${language}`;
-    let data;
-    try {
-      const response = await fetch(url);
-      data = await response.json();
-    } catch (e) {
-      console.error(e);
-    }
-    processOpenCageDataResponse(data);
-  }
-
-  async function getLocationByCity(cityInput) {
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${cityInput}&key=${opencagedataAPIkey}&language=${language}`;
-    let data;
-    try {
-      const response = await fetch(url);
-      data = await response.json();
-    } catch (e) {
-      console.error(e);
-    }
-    processOpenCageDataResponse(data);
-  }
-
-  async function processOpenCageDataResponse(APIResponse) {
-    updateCoordinates(APIResponse);
-
-    timezone = APIResponse.results[0].annotations.timezone.name;
-    locationInfo = {
-      ...extractDate(timezone),
-      latitude,
-      longitude,
-    };
-
-    forecast = await getForecast(locationInfo);
-    const imageData = await fetchFlickrImage(locationInfo, forecast);
-    showImage(imageData.backgroundImageUrl);
-    showCurrentForecast(forecast);
-    renderLocation(APIResponse);
-    updateMap(locationInfo);
-  }
-
-  function updateCoordinates(APIResponse) {
-    latitude = APIResponse.results[0].geometry.lat;
-    longitude = APIResponse.results[0].geometry.lng;
-  }
-
-  function updateMap(locationInfo) {
-    map.setCenter([locationInfo.longitude, locationInfo.latitude]);
-    const translator = new Translator(language);
-    const latitude = translator.get('lat');
-    const longitude = translator.get('lon');
-    document.querySelector('.latitude').innerHTML = `${latitude}${Math.trunc(locationInfo.latitude)}°`;
-    document.querySelector('.longitude').innerHTML = `${longitude}${Math.trunc(locationInfo.longitude)}'`;
-  }
-
-  function renderLocation(APIResponse) {
-    const city = APIResponse.results[0].components.city || APIResponse.results[0].components.state;
-    const { country } = APIResponse.results[0].components;
-
-    document.querySelector('.location').innerHTML = `${city}, ${country}`;
-  }
 
   function extractDate(timezone) {
     const utcDate = new Date();
@@ -184,71 +79,30 @@ window.onload = () => {
     document.querySelector('.date').innerHTML = `${date}   ${time}`;
   }
 
-  async function getForecast(locationInfo) {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    let targetUrl = `https://api.darksky.net/forecast/${darkskyAPIkey}/${locationInfo.latitude},${locationInfo.longitude}?lang=${language}`;
-    if (temperatureType === 'celcius') {
-      targetUrl += '&units=si';
-      highlightTempButton(celcButton, fahrButton);
-    } else {
-      targetUrl += '&units=us';
-      highlightTempButton(fahrButton, celcButton);
-    }
+  setInterval(() => {
+    const dateInfo = extractDate(timezone);
+    showDate(dateInfo.utcDateArr, dateInfo.localDateArr);
+  }, 1000);
 
-    try {
-      const response = await fetch(proxyUrl + targetUrl);
-      var data = await response.json();
-    } catch (e) {
-      console.error(e);
-    }
-
-    return extractForecastData(data);
+  function updateCoordinates(APIResponse) {
+    latitude = APIResponse.results[0].geometry.lat;
+    longitude = APIResponse.results[0].geometry.lng;
   }
 
-  function highlightTempButton(trueButton, falseButton) {
-    falseButton.classList.remove('highlighted');
-    trueButton.classList.add('highlighted');
-  }
-
-  function extractForecastData(APIResponse) {
+  function updateMap(locationInfo) {
+    map.setCenter([locationInfo.longitude, locationInfo.latitude]);
     const translator = new Translator(language);
-    const tempFeels = translator.get('temp');
-    const wind = translator.get('wind');
-    const humidity = translator.get('hum');
-
-    const forecast = {};
-    forecast.temperature = `${Math.round(APIResponse.currently.temperature)}°`;
-    forecast.summary = `${APIResponse.currently.summary}`;
-    forecast.apparentTemp = `${tempFeels}${Math.round(APIResponse.currently.apparentTemperature)}°`;
-    forecast.wind = `${wind}${APIResponse.currently.windSpeed} m/s`;
-    forecast.humidity = `${humidity}${Math.round(APIResponse.currently.humidity * 100)}%`;
-    forecast.weatherState = APIResponse.currently.icon;
-    const iconURL = icons[forecast.weatherState];
-    forecast.iconURL = `url(/dist/${iconURL})`;
-    forecast.days = extract3DayForecast(APIResponse);
-    return forecast;
+    const latitude = translator.get('lat');
+    const longitude = translator.get('lon');
+    document.querySelector('.latitude').innerHTML = `${latitude}${Math.trunc(locationInfo.latitude)}°`;
+    document.querySelector('.longitude').innerHTML = `${longitude}${Math.trunc(locationInfo.longitude)}'`;
   }
 
-  function showCurrentForecast(forecast) {
-    document.querySelector('.currentWeather--temperature').innerHTML = forecast.temperature;
-    document.querySelector('.description--summary').innerHTML = forecast.summary;
-    document.querySelector('.description--apparentTemp').innerHTML = forecast.apparentTemp;
-    document.querySelector('.description--wind').innerHTML = forecast.wind;
-    document.querySelector('.description--humidity').innerHTML = forecast.humidity;
-    document.querySelector('.currentWeather--image').style.backgroundImage = forecast.iconURL;
-    renderDaysForecast(forecast.days);
-  }
+  function renderLocation(APIResponse) {
+    const city = APIResponse.results[0].components.city || APIResponse.results[0].components.state;
+    const { country } = APIResponse.results[0].components;
 
-  function extract3DayForecast(APIResponse) {
-    return [APIResponse.daily.data[1], APIResponse.daily.data[2], APIResponse.daily.data[3]];
-  }
-
-  function renderDaysForecast(days) {
-    days.forEach((day, index) => {
-      renderWeekday(day, index + 1);
-      renderTemperature(day, index + 1);
-      renderWeatherIcons(day, index + 1);
-    });
+    document.querySelector('.location').innerHTML = `${city}, ${country}`;
   }
 
   function renderWeekday(day, index) {
@@ -278,37 +132,64 @@ window.onload = () => {
     document.querySelector(`.weatherForecast--day${index}--image`).style.backgroundImage = `url(/dist/${iconURL})`;
   }
 
-  async function fetchFlickrImage(location, forecast) {
-    const baseUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPIkey}`;
-    const params = `&lat=${location.latitude}&lon=${location.longitude}&tags=${location.partOfDay},${location.season},${forecast.weatherState}&geo_context=2&format=json&nojsoncallback=1&extras=url_o`;
-    const url = baseUrl + params;
+  function renderDaysForecast(days) {
+    days.forEach((day, index) => {
+      renderWeekday(day, index + 1);
+      renderTemperature(day, index + 1);
+      renderWeatherIcons(day, index + 1);
+    });
+  }
 
+  function showCurrentForecast(forecast) {
+    document.querySelector('.currentWeather--temperature').innerHTML = forecast.temperature;
+    document.querySelector('.description--summary').innerHTML = forecast.summary;
+    document.querySelector('.description--apparentTemp').innerHTML = forecast.apparentTemp;
+    document.querySelector('.description--wind').innerHTML = forecast.wind;
+    document.querySelector('.description--humidity').innerHTML = forecast.humidity;
+    document.querySelector('.currentWeather--image').style.backgroundImage = forecast.iconURL;
+    renderDaysForecast(forecast.days);
+  }
+
+  async function processOpenCageDataResponse(APIResponse) {
+    updateCoordinates(APIResponse);
+
+    timezone = APIResponse.results[0].annotations.timezone.name;
+    locationInfo = {
+      ...extractDate(timezone),
+      latitude,
+      longitude,
+    };
+
+    forecast = await getForecast(locationInfo);
+    const imageData = await fetchFlickrImage(locationInfo, forecast);
+    showImage(imageData.backgroundImageUrl);
+    showCurrentForecast(forecast);
+    renderLocation(APIResponse);
+    updateMap(locationInfo);
+  }
+
+  async function getLocationByCity(cityInput) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${cityInput}&key=${opencagedataAPIkey}&language=${language}`;
+    let data;
     try {
       const response = await fetch(url);
-      const data = await response.json();
-
-      return {
-        backgroundImageUrl: extractImageUrl(data),
-      };
+      data = await response.json();
     } catch (e) {
       console.error(e);
     }
+    processOpenCageDataResponse(data);
   }
 
-  function extractImageUrl(APIResponse) {
-    const imageNumber = Math.floor(Math.random() * APIResponse.photos.photo.length);
-    const image = APIResponse.photos.photo[imageNumber];
-    return image.url_o || image.url_h || `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_b.jpg`;
-  }
-
-  async function loadImage() {
-    const imageData = await fetchFlickrImage(locationInfo, forecast);
-    showImage(imageData.backgroundImageUrl);
-  }
-
-  function showImage(imageURL) {
-    const image = document.querySelector('.image');
-    image.style.backgroundImage = `url(${imageURL})`;
+  async function getLocationByCoordinates(latitude, longitude) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${opencagedataAPIkey}&language=${language}`;
+    let data;
+    try {
+      const response = await fetch(url);
+      data = await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+    processOpenCageDataResponse(data);
   }
 
   async function getPositionByIP() {
@@ -324,6 +205,104 @@ window.onload = () => {
 
     getLocationByCity(city);
   }
+
+  async function success(pos) {
+    if (pos) {
+      const crd = pos.coords;
+      const latitude = crd.latitude.toFixed(2);
+      const longitude = crd.longitude.toFixed(2);
+      await getLocationByCoordinates(latitude, longitude);
+    } else {
+      getPositionByIP();
+    }
+  }
+
+  navigator.geolocation.getCurrentPosition(success);
+
+  function highlightTempButton(trueButton, falseButton) {
+    falseButton.classList.remove('highlighted');
+    trueButton.classList.add('highlighted');
+  }
+
+  function extract3DayForecast(APIResponse) {
+    return [APIResponse.daily.data[1], APIResponse.daily.data[2], APIResponse.daily.data[3]];
+  }
+
+  function extractForecastData(APIResponse) {
+    const translator = new Translator(language);
+    const tempFeels = translator.get('temp');
+    const wind = translator.get('wind');
+    const humidity = translator.get('hum');
+
+    const forecast = {};
+    forecast.temperature = `${Math.round(APIResponse.currently.temperature)}°`;
+    forecast.summary = `${APIResponse.currently.summary}`;
+    forecast.apparentTemp = `${tempFeels}${Math.round(APIResponse.currently.apparentTemperature)}°`;
+    forecast.wind = `${wind}${APIResponse.currently.windSpeed} m/s`;
+    forecast.humidity = `${humidity}${Math.round(APIResponse.currently.humidity * 100)}%`;
+    forecast.weatherState = APIResponse.currently.icon;
+    const iconURL = icons[forecast.weatherState];
+    forecast.iconURL = `url(/dist/${iconURL})`;
+    forecast.days = extract3DayForecast(APIResponse);
+    return forecast;
+  }
+
+  async function getForecast(locationInfo) {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    let targetUrl = `https://api.darksky.net/forecast/${darkskyAPIkey}/${locationInfo.latitude},${locationInfo.longitude}?lang=${language}`;
+    if (temperatureType === 'celcius') {
+      targetUrl += '&units=si';
+      highlightTempButton(celcButton, fahrButton);
+    } else {
+      targetUrl += '&units=us';
+      highlightTempButton(fahrButton, celcButton);
+    }
+
+    let data;
+    try {
+      const response = await fetch(proxyUrl + targetUrl);
+      data = await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+
+    return extractForecastData(data);
+  }
+
+  function extractImageUrl(APIResponse) {
+    const imageNumber = Math.floor(Math.random() * APIResponse.photos.photo.length);
+    const image = APIResponse.photos.photo[imageNumber];
+    return image.url_o || image.url_h || `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_b.jpg`;
+  }
+
+  async function fetchFlickrImage(location, forecast) {
+    const baseUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPIkey}`;
+    const constantParams = '&geo_context=2&format=json&nojsoncallback=1&extras=url_o';
+    const customParams = `&lat=${location.latitude}&lon=${location.longitude}&tags=${location.partOfDay},${location.season},${forecast.weatherState}`;
+    const url = baseUrl + customParams + constantParams;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return {
+        backgroundImageUrl: extractImageUrl(data),
+      };
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function showImage(imageURL) {
+    const image = document.querySelector('.image');
+    image.style.backgroundImage = `url(${imageURL})`;
+  }
+
+  async function loadImage() {
+    const imageData = await fetchFlickrImage(locationInfo, forecast);
+    showImage(imageData.backgroundImageUrl);
+  }
+
 
   function recogniseSpeech() {
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -341,6 +320,32 @@ window.onload = () => {
     recognition.addEventListener('end', recognition.start);
     recognition.start();
   }
+
+  city.addEventListener('click', () => recogniseSpeech());
+  city.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+      getLocationByCity(city.value);
+    }
+  });
+
+  const languageButton = document.querySelector('select[class=languages]');
+
+  async function languageHandler() {
+    language = languageButton.value.toLowerCase();
+    localStorage.setItem('lang', language);
+    await getLocationByCoordinates(latitude, longitude);
+  }
+
+  languageButton.addEventListener('change', () => { languageHandler(); });
+  languageButton.value = language.toUpperCase();
+
+  searchCityButton.addEventListener('click', () => { getLocationByCity(city.value); city.value = ''; });
+
+  fahrButton.addEventListener('click', async () => { temperatureType = 'fahrenheit'; showCurrentForecast(await getForecast({ latitude, longitude })); });
+  celcButton.addEventListener('click', async () => { temperatureType = 'celcius'; showCurrentForecast(await getForecast({ latitude, longitude })); });
+
+  const refreshButton = document.querySelector('input[name=refresh]');
+  refreshButton.addEventListener('click', () => { loadImage(); });
 };
 
 window.onbeforeunload = () => {
